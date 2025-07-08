2a1ccf2cf6f0337bfa52f908dbc41e1be57535b5
Khalid Benjelloun <khalidmailing@gmail.com>
2025-07-08 20:52:48 +0100

    ✅ add: AreaCalculator.java

---
+++ Coding Exercise 09 Constructing A Comprehensive Area Calculator For Circles And Rectangles In Java/AreaCalculator.java
@@ -0,0 +1,16 @@
+public class AreaCalculator {
+    public static double area(double radius) {
+        if (radius >= 0) {
+            return Math.PI * Math.pow(radius, 2);
+        }
+        return -1;
+    }
+    
+        public static double area(double x, double y) {
+        if (x >= 0 && y >= 0) {
+            return x * y;
+        }
+        return -1;
+    }
+}
+
