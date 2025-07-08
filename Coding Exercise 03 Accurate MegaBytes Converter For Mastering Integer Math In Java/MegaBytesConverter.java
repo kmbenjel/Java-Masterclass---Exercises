ff51382705464df7c23fd2c8583fd38bd0513354
Khalid Benjelloun <khalidmailing@gmail.com>
2025-07-08 12:43:44 +0100

    ✅ add: MegaBytesConverter.java

--- Coding Exercise 03 Accurate MegaBytes Converter For Mastering Integer Math In Java/MegaBytesConverter.java
+++ Coding Exercise 03 Accurate MegaBytes Converter For Mastering Integer Math In Java/MegaBytesConverter.java
@@ -1,1 +1,9 @@
-a
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        System.out.println(
+            kiloBytes < 0 ?
+                "Invalid Value"
+                    : kiloBytes + " KB = " + kiloBytes / 1024 + " MB and " + kiloBytes % 1024 + " KB"
+            );
+    } 
+}
