1316bee4b9622f0c236964af94659c128101a4fb
Khalid Benjelloun <khalidmailing@gmail.com>
2025-07-19 21:35:04 +0100

    ✅ add: CompositionChallenge

---
+++ CompositionChallenge/src/SmartKitchen.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    private double width;
+    private double length;
+
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+}
